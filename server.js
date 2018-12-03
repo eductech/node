908ecba9e9536ba92750cafc111ecf95729b9b97
alex-zamirouski <a.zamirouski@godeltech.com>
2018-12-03 17:22:12 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const mime = require('mime')
+const cache = {}
+
+const send404 = (response) => {
+  response.writeHead(404, {'Content-Type': 'text/plain'})
+  response.write('Error 404: resource not found.')
+  response.end()
+}
+
+const sendFile = (response, filePath, fileContents) => {
+  response.writeHead(
+    200,
+    {'content-type': mime.lookup(path.basename(filePath))}
+  )
+  response.end(fileContents)
+}
+
+const serveStatic = (response, cach, absPath) => {
+  if (cach[absPath]) {
+    sendFile(response, absPath, cache[absPath])
+  } else {
+    fs.exists(absPath, (exists) => {
+      if (exists) {
+        fs.readFile(absPath, (err, data) => {
+          if (err) {
+            send404(response)
+          } else {
+            cach[absPath] = data
+            sendFile(response, absPath, data)
+          }
+        })
+      } else {
+        send404(response)
+      }
+    })
+  }
+}
