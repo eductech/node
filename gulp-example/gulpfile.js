29613c4d3904b97357d1299ba2f85981af968a06
alex-zamirouski <a.zamirouski@godeltech.com>
2018-12-14 16:59:17 +0300

    gulp-watch finished

--- gulp-example/gulpfile.js
+++ gulp-example/gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp')
+const watch = require('gulp-watch')
 const sourcemaps = require('gulp-sourcemaps')
 const babel = require('gulp-babel')
 const concat = require('gulp-concat')
@@ -7,9 +8,13 @@ gulp.task('default', () => {
   return gulp.src('app/*.jsx')
     .pipe(sourcemaps.init())
     .pipe(babel({
-      presets: ['env', 'react']
+      presets: ['@babel/preset-env', '@babel/preset-react']
     }))
     .pipe(concat('all.js'))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'))
+})
+
+gulp.task('watch', () => {
+  watch('app/**.jsx', () => gulp.start('default'))
 })
